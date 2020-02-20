e59fb8da6a0e79d13a589adb26223dc70f9aae46
weiqing.twq <weiqing.twq@antfin.com>
2020-02-20 23:27:26 +0800

    Updates

---
+++ precache-manifest.33e64ad8a19e5797ef8e18acdf98916f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863d97635539c0b34f08d37346cfe55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9af2f93e73a2cc8620",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f58c567a410b84a4c8e",
+    "url": "/static/js/2.ca87ba5c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ca87ba5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca9af2f93e73a2cc8620",
+    "url": "/static/js/main.3a928762.chunk.js"
+  },
+  {
+    "revision": "b0998427be409f6d3a78",
+    "url": "/static/js/runtime-main.e1624ac6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
