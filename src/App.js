d3f521851d7b4500064696c6f10305f4ff9e58a7
weiqing.twq <weiqing.twq@antfin.com>
2020-02-22 15:28:30 +0800

    feat:引入farbric

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
+import { fabric } from 'fabric'
 
 export default class Canvas extends React.Component {
+
   componentDidMount() {
-    const canvas = this.refs.bigcanvas
+    const canvas = document.getElementById('c');
+    const fabricCanvas = new fabric.Canvas(canvas, { width: 1024, height: 1024, backgroundColor: '#C6CDD5' });
+    var rect = new fabric.Rect({
+      top: 0,
+      left: 0,
+      width: 100,
+      height: 200,
+      fill: 'red'
+    });
+    fabricCanvas.add(rect);
   }
+
   render() {
     return (
       <div className='bcContainer'>
-        <canvas className='bigcanvas' id='bigcanvas' ref="bigcanvas" />
+        <canvas className='c' id='c' />
       </div>
     )
   }
