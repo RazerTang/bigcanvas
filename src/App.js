216cc8ba4335d0da7b8b831edcf61a3fa9b662f7
weiqing.twq <weiqing.twq@antfin.com>
2020-02-24 00:05:34 +0800

    feat:v0.0.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './App.css';
 import { fabric } from 'fabric'
+import Post from './Post.js'
 
 export default class Canvas extends React.Component {
 
   componentDidMount() {
     const canvas = document.getElementById('c');
     const fabricCanvas = new fabric.Canvas(canvas, { width: 1024, height: 1024, backgroundColor: '#C6CDD5' });
-    var rect = new fabric.Rect({
-      top: 0,
-      left: 0,
-      width: 100,
-      height: 200,
-      fill: 'red'
+
+
+    fabric.util.addListener(document.body, 'keydown', function (options) {
+      if (options.repeat) {
+        return;
+      }
+      var key = options.which || options.keyCode; // key detection
+      if (key === 8) {
+        fabricCanvas.remove(fabricCanvas.getActiveObject());
+      }
     });
-    fabricCanvas.add(rect);
+
+    fabricCanvas.on('mouse:dblclick', (e) => {
+      if (!e.target) {
+        var rect = new Post(e.pointer);
+        fabricCanvas.add(rect);
+        fabricCanvas.setActiveObject(rect);
+      } else {
+      }
+    });
   }
 
   render() {
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import { fabric } from 'fabric'
+
+
+export default class Post extends fabric.Rect {
+    constructor(props) {
+        super({
+            top: props.y - 50,
+            left: props.x - 50,
+            width: 100,
+            height: 100,
+            fill: '#FFF09A'
+        });
+    }
+
+    onSelect(){
+        this.backgroundColor = '#00A2FF';
+    }
+
+    onDeselect(){
+        this.backgroundColor = '#00A2FF00';
+    }
+}
