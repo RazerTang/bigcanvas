7122c5c9bf62dc4e57c69c2afa4b0531687b80de
weiqing.twq <weiqing.twq@antfin.com>
2020-02-27 22:44:30 +0800

    feat:文字编辑初步

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ export default class Canvas extends React.Component {
       if (options.repeat) {
         return;
       }
+
       var key = options.which || options.keyCode; // key detection
+
+      if (fabricCanvas.getActiveObject()) {
+        const post = fabricCanvas.getActiveObject().data;
+        if (post) {
+          post.active();
+        }
+      }
+      //在文字编辑状态是不能删除post
       if (key === 8) {
         fabricCanvas.remove(fabricCanvas.getActiveObject());
       }
@@ -22,13 +31,41 @@ export default class Canvas extends React.Component {
 
     fabricCanvas.on('mouse:dblclick', (e) => {
       if (!e.target) {
-        var rect = new Post(e.pointer);
-        fabricCanvas.add(rect);
-        fabricCanvas.setActiveObject(rect);
+        var post = new Post({ x: e.pointer.x, y: e.pointer.y, canvas: fabricCanvas });
       } else {
       }
     });
+    // fabricCanvas.getActiveObject().toGroup();
+
+    // fabricCanvas.on({
+    //   'touch:gesture': function () {
+    //   },
+    //   'touch:drag': function () {
+    //     alert('move');
+    //   },
+    //   'touch:orientation': function () {
+    //   },
+    //   'touch:shake': function () {
+    //   },
+    //   'touch:longpress': function () {
+    //   }
+    // });
+
+    // fabricCanvas.on('mouse:up', (e) => {
+    //   alert('up');
+    // });
+
+
+    // let textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+    //   left: 50,
+    //   top: 50,
+    //   width: 150,
+    //   fontSize: 20
+    // });
+    // let group = new fabric.Group([textbox]);
+    // fabricCanvas.add(group);
   }
+
 
   render() {
     return (
--- src/Post.js
+++ src/Post.js
@@ -1,22 +1,113 @@
 import { fabric } from 'fabric'
 
+// fabric.Text.prototype.calcTextHeight = function () {
+//     var lineHeight, height = 0;
+//     for (var i = 0, len = this._textLines.length; i < len; i++) {
+//         lineHeight = this.getHeightOfLine(i);
+//         height += lineHeight;
+//     }
+//     return height;
+// };
 
-export default class Post extends fabric.Rect {
+// fabric.Text.prototype.getMaxTextWidth = function () {
+//     var maxWidth = this.getLineWidth(0)
+//     for (var i = 0, len = this._textLines.length; i < len; i++) {
+//         maxWidth = Math.max(maxWidth, this.getLineWidth(i));
+
+//     }
+//     return maxWidth;
+// }
+
+
+
+export default class Post {
+
     constructor(props) {
-        super({
-            top: props.y - 50,
-            left: props.x - 50,
-            width: 100,
-            height: 100,
-            fill: '#FFF09A'
+        this.width = 100;
+        this.height = 100;
+        this.lines = 2;
+
+        this.init(props);
+        this.textbox.on('mousedown', (e) => {
+            this.active();
         });
+
+        this.textbox.on('editing:exited', (e) => {
+            this.unactive();
+        });
+
     }
 
-    onSelect() {
-        this.backgroundColor = '#00A2FF';
+    init(props) {
+        this.canvas = props.canvas;
+        const frame = {
+            originX: 'center',
+            originY: 'center',
+            top: props.y,
+            left: props.x,
+            width: this.width,
+            height: this.height
+        };
+
+        this.rect = new fabric.Rect({
+            ...frame,
+            fill: '#FFF09A',
+            hasControls: false,
+        });
+
+        const LimitedTextbox = fabric.util.createClass(fabric.Textbox, {
+
+        });
+
+
+        this.textbox = new LimitedTextbox('', {
+            ...frame,
+            width: this.width,
+            top: props.y,
+            fontSize: 30,
+            // textAlign: 'center',
+            // textBaseline: 'middle',
+            fill: '#000',
+            hasBorders: false,
+            breakWords: true,
+        });
+
+
+        this.group = new fabric.Group();
+        this.group.data = this;
+
+        this.active();
     }
 
-    onDeselect() {
-        this.backgroundColor = '#00A2FF00';
+    /**
+     * active可以激活的两个图形
+     * 1.创建的时候
+     * 2.选中该group并输入文字的时候
+     */
+    active() {
+        this.group.removeWithUpdate(this.rect);
+        this.group.removeWithUpdate(this.textbox);
+        this.canvas.remove(this.group);
+
+        this.canvas.add(this.rect);
+        this.canvas.add(this.textbox);
+        this.canvas.setActiveObject(this.textbox);
+
+        this.rect.backgroundColor = '#00A2FF';
+        this.textbox.enterEditing();
+    }
+
+    /**
+     * unactive的条件为文字编辑状态消失的时候
+     */
+    unactive() {
+        this.canvas.remove(this.rect);
+        this.canvas.remove(this.textbox);
+
+        this.group.addWithUpdate(this.rect);
+        this.group.addWithUpdate(this.textbox);
+        this.canvas.add(this.group);
+
+        this.rect.backgroundColor = '#00A2FF00';
     }
 }
